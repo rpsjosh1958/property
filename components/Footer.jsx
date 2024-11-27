34b355f5d3bb3e755ed538ab52204155e6cf7026
ArindamKahsyap0 <115341049+ArindamKahsyap0@users.noreply.github.com>
2024-11-28 01:48:43 +0530

    v1.0

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => (
+    <Box textAlign='center' p='5' color='gray.600'>
+        Made by Arindam Kashyap by fetching data from Rapid api
+        <br />
+        &copy; 2024 
+    </Box>
+);
+
+export default Footer;
